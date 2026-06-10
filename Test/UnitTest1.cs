d63e9f6eccb21683ae2fa09818cc348140de58ea
Ucse <Ucse>
2026-06-09 21:56:54 -0300

    Agregar cambios en el archivo 'Test'

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,6 +1,10 @@
+using NUnit.Framework;
+using ClinicaVital;
+
 namespace Test;
 
-public class Tests
+[TestFixture]
+public class ClinicaVitalTests
 {
     [SetUp]
     public void Setup()
@@ -8,8 +12,23 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TurnoPorUrgencia_NivelUrgenciaAlta_CalculeTiempoDeEspera15Minutos_Y_NivelUrgenciaCritica_Calcule0Minutos()
     {
-        Assert.Pass();
+        var tiempoDeEsperaAlta = 0;
+        var turnoPorUrgenciaAlta = new TurnoPorUrgencia(1234, "Uriel", 20, "Dolor de cabeza", "Alta");
+        turnoPorUrgenciaAlta.CalcularTiempoDeEspera(tiempoDeEsperaAlta);
+        Assert.That(turnoPorUrgenciaAlta.CalcularTiempoDeEspera(tiempoDeEsperaAlta), Is.EqualTo(15));
+
+        var tiempoDeEsperaCritica = 0;
+        var turnoPorUrgenciaCritica = new TurnoPorUrgencia(5647, "Fabricio", 11, "Dolor de estómago", "Critica");
+        turnoPorUrgenciaCritica.CalcularTiempoDeEspera(tiempoDeEsperaCritica);
+        Assert.That(turnoPorUrgenciaCritica.CalcularTiempoDeEspera(tiempoDeEsperaCritica), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void EstaEnEspera_DevuelveTrueCuandoUnPacienteTieneUnTurnoRegistrado_Y_FalseCuandoNo()
+    {
+        var turno = new TurnoPorUrgencia(1234, "Juan", 20, "Corte", "Bajo");
+        var salaDeEspera = new SalaDeEspera();
     }
 }
